30fd7765ae43af898147a0e5b49df1748247c78d
Monte Cristo <misterwu1998@163.com>
2023-03-16 18:46:57 +0800

    封装为单个函数

--- ga.py
+++ ga.py
@@ -1,14 +1,6 @@
 import config as conf
 import random
-
-city_dist_mat = None
-config = conf.get_config()
-# 各项参数
-gene_len = config.city_num
-individual_num = config.individual_num
-gen_num = config.gen_num
-mutate_prob = config.mutate_prob
-
+import numpy as np
 
 def copy_list(old_arr: [int]):
     new_arr = []
@@ -19,45 +11,49 @@ def copy_list(old_arr: [int]):
 
 # 个体类
 class Individual:
-    def __init__(self, genes=None):
+    def __init__(self, gene_len,distMat, genes=None):
         # 随机生成序列
         if genes is None:
             genes = [i for i in range(gene_len)]
             random.shuffle(genes)
+        self.gene_len = gene_len
         self.genes = genes
+        self.city_dist_mat = distMat
         self.fitness = self.evaluate_fitness()
 
     def evaluate_fitness(self):
         # 计算个体适应度
         fitness = 0.0
-        for i in range(gene_len - 1):
+        for i in range(self.gene_len - 1):
             # 起始城市和目标城市
             from_idx = self.genes[i]
             to_idx = self.genes[i + 1]
-            fitness += city_dist_mat[from_idx, to_idx]
+            fitness += self.city_dist_mat[from_idx, to_idx]
         # 连接首尾
-        fitness += city_dist_mat[self.genes[-1], self.genes[0]]
+        fitness += self.city_dist_mat[self.genes[-1], self.genes[0]]
         return fitness
 
 
 class Ga:
-    def __init__(self, input_):
-        global city_dist_mat
-        city_dist_mat = input_
+    def __init__(self, nIndividuals,input_,mutationProbability):
+        self.city_dist_mat = input_
         self.best = None  # 每一代的最佳个体
         self.individual_list = []  # 每一代的个体列表
         self.result_list = []  # 每一代对应的解
         self.fitness_list = []  # 每一代对应的适应度
+        self.gene_len = np.shape(input_)[0]
+        self.individual_num = nIndividuals
+        self.mutate_prob = mutationProbability
 
     def cross(self):
         new_gen = []
         random.shuffle(self.individual_list)
-        for i in range(0, individual_num - 1, 2):
+        for i in range(0, self.individual_num - 1, 2):
             # 父代基因
             genes1 = copy_list(self.individual_list[i].genes)
             genes2 = copy_list(self.individual_list[i + 1].genes)
-            index1 = random.randint(0, gene_len - 2)
-            index2 = random.randint(index1, gene_len - 1)
+            index1 = random.randint(0, self.gene_len - 2)
+            index2 = random.randint(index1, self.gene_len - 1)
             pos1_recorder = {value: idx for idx, value in enumerate(genes1)}
             pos2_recorder = {value: idx for idx, value in enumerate(genes2)}
             # 交叉
@@ -68,17 +64,17 @@ class Ga:
                 genes2[j], genes2[pos2] = genes2[pos2], genes2[j]
                 pos1_recorder[value1], pos1_recorder[value2] = pos1, j
                 pos2_recorder[value1], pos2_recorder[value2] = j, pos2
-            new_gen.append(Individual(genes1))
-            new_gen.append(Individual(genes2))
+            new_gen.append(Individual(self.gene_len,self.city_dist_mat,genes1))
+            new_gen.append(Individual(self.gene_len,self.city_dist_mat,genes2))
         return new_gen
 
     def mutate(self, new_gen):
         for individual in new_gen:
-            if random.random() < mutate_prob:
+            if random.random() < self.mutate_prob:
                 # 翻转切片
                 old_genes = copy_list(individual.genes)
-                index1 = random.randint(0, gene_len - 2)
-                index2 = random.randint(index1, gene_len - 1)
+                index1 = random.randint(0, self.gene_len - 2)
+                index2 = random.randint(index1, self.gene_len - 1)
                 genes_mutate = old_genes[index1:index2]
                 genes_mutate.reverse()
                 individual.genes = old_genes[:index1] + genes_mutate + old_genes[index2:]
@@ -89,14 +85,14 @@ class Ga:
         # 锦标赛
         group_num = 10  # 小组数
         group_size = 10  # 每小组人数
-        group_winner = individual_num // group_num  # 每小组获胜人数
+        group_winner = self.individual_num // group_num  # 每小组获胜人数
         winners = []  # 锦标赛结果
         for i in range(group_num):
             group = []
             for j in range(group_size):
                 # 随机组成小组
                 player = random.choice(self.individual_list)
-                player = Individual(player.genes)
+                player = Individual(self.gene_len,self.city_dist_mat,player.genes)
                 group.append(player)
             group = Ga.rank(group)
             # 取出获胜者
@@ -124,12 +120,12 @@ class Ga:
             if individual.fitness < self.best.fitness:
                 self.best = individual
 
-    def train(self):
+    def train(self, nIterations):
         # 初代种群
-        self.individual_list = [Individual() for _ in range(individual_num)]
+        self.individual_list = [Individual(self.gene_len,self.city_dist_mat) for _ in range(self.individual_num)]
         self.best = self.individual_list[0]
         # 迭代
-        for i in range(gen_num):
+        for i in range(nIterations):
             self.next_gen()
             # 连接首尾
             result = copy_list(self.best.genes)
@@ -137,3 +133,32 @@ class Ga:
             self.result_list.append(result)
             self.fitness_list.append(self.best.fitness)
         return self.result_list, self.fitness_list
+
+def solveTSP(
+    distMat
+    , addVirtualSourcePoint=False
+    , nIndividuals=50
+    , nIterations=500
+    , mutationProbability=0.25):
+    '''
+        distMat[i,j]: i号到j号的距离
+        addVirtualSourcePoint: 要不要加一个虚拟源点，它到任意点的距离都是0；算法得到的路线默认是闭环的，加了这个虚拟源点就是开环的
+        return: 路径上各点序号[], 适应度曲线[]
+    '''
+    if addVirtualSourcePoint:
+        newShape = (np.array(np.shape(distMat))+1).astype('int')
+        newDistMat = np.zeros(newShape)
+        newDistMat[1:,1:] = distMat
+        distMat = newDistMat
+    ga = Ga(nIndividuals,distMat,mutationProbability)
+    ret = ga.train(nIterations)
+    result = list(ret[0][-1])
+    fitness_list = ret[1]
+    if addVirtualSourcePoint:
+        # 抽走多出来的那个虚拟点
+        result.remove(0)
+        for i in range(len(result)):
+            result[i] -= 1
+        # 抽走最后的闭环起点
+        result.pop(-1)
+    return result, fitness_list
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import config as conf
-from ga import Ga
+from ga import Ga, solveTSP
 import matplotlib.pyplot as plt
 
 config = conf.get_config()
@@ -19,17 +19,15 @@ def build_dist_mat(input_list):
 
 
 # 城市坐标
-city_pos_list = np.random.rand(config.city_num, config.pos_dimension)
+city_pos_list = np.random.rand(config.city_num, config.pos_dimension) # 城市个数×2
 # 城市距离矩阵
-city_dist_mat = build_dist_mat(city_pos_list)
+city_dist_mat = build_dist_mat(city_pos_list) # 二维数组[i,j]: i号到j号的距离
 
 print(city_pos_list)
 print(city_dist_mat)
 
 # 遗传算法运行
-ga = Ga(city_dist_mat)
-result_list, fitness_list = ga.train()
-result = result_list[-1]
+result,fitness_list = solveTSP(city_dist_mat, True, nIterations=400)
 result_pos_list = city_pos_list[result, :]
 
 # 绘图
@@ -37,14 +35,16 @@ result_pos_list = city_pos_list[result, :]
 plt.rcParams['font.sans-serif'] = ['KaiTi']  # 指定默认字体
 plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
 
-fig = plt.figure()
+# fig = plt.figure()
 plt.plot(result_pos_list[:, 0], result_pos_list[:, 1], 'o-r')
 plt.title(u"路线")
 plt.legend()
-fig.show()
+# fig.show()
+plt.show()
 
-fig = plt.figure()
+# fig = plt.figure()
 plt.plot(fitness_list)
 plt.title(u"适应度曲线")
 plt.legend()
-fig.show()
+# fig.show()
+plt.show()
